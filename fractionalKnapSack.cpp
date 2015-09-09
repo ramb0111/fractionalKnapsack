927210cc1eb475c990b9d03c95fe081c55ac1bc1
ankur <ankur.12cs133@abes.ac.in>
2015-09-09 21:35:10 +0530

    file added

---
+++ fractionalKnapSack.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+ {
+   /* Program of fractional KnapSack implementation
+    i/p: size and value of each product along with the capacity of the Sack
+    o/p: maximize the value corresponding to capacity
+    */
+
+    int num,capacity,ans=0;
+    cout<<"Enter the number of items:\n";
+    cin>>num;
+    multimap <float,float,greater<int> > m;
+    cout<<"\nEnter the size along with the values:\n";
+    float siz,value;
+    for(int i=0;i<num;++i)
+         {cin>>siz>>value;
+          m.insert(pair<int,int>(value/siz,siz));
+         }
+    cout<<"\nEnter the capacity of the Sack:\n";
+    cin>>capacity;
+    multimap<float, float> :: iterator itr;
+    for(itr=m.begin();itr!=m.end();++itr)
+    {   int temp=itr->second;
+        if(temp<capacity)
+            {ans+= temp*(itr->first);
+             capacity -=temp;
+            }
+       else {ans+=capacity*(itr->first);
+             capacity=0;
+             break;
+             }
+    }
+   cout<<"\n\nThe maximum possible value is: "<<ans<<"\n";
+ return 0;
+ }
